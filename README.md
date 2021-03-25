82865b5e54e67c0ef58e8ce07596f84309b6f065
VArtamonov <avv200071@mail.ru>
2021-03-25 23:20:46 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# MY_GIT_MASTER
+
+Привет всем. Hi everyone. :wave:
+
+Пока все изложу все кратко. 
+So far, I will state everything briefly.
+
+Главный Архив
+
+Набор личных программ для автоматизации работы с Git. Используется как альтернатива архиваторам.
+Работа производиться в Microsoft Windows [https://www.microsoft.com/ru-ru/windows]
+
+Будет использоваться для сохранения моих программ на GitHub. 
+Позволяет, через запуск одного файла (в Windows командного файла):
+ - создовать репозитарий (repo) Git локально
+ - добавлять файлы в repo
+ - делать автоматический коммит с привязкой к времени
+ - отправить все изменения на GitHub (обновление удаленной ветки)
+
+So far, I will state everything briefly.
+A set of personal programs for automating work with Git. Used as an alternative to the archiver.
+The work is done in Microsoft Windows [https://www.microsoft.com/ru-ru/windows]
+
+Используемые программы, ссылки:
+Programs used, links:
+1. **Git for Windows**	https://git-scm.com/download/win
+2. **GitHub CLI**	https://cli.github.com/
+3. **Far Manager**	https://farmanager.com/
+4. **MSYS2**		https://www.msys2.org/
+
+Кое какие ссылки:
+ How to echo with different colors in the Windows command line https://stackoverflow.com/questions/2048509/how-to-echo-with-different-colors-in-the-windows-command-line
+
+____
+УВЫ. ALAS! :raised_hands:
+Перевод делал через Яндекс Переводчик.
+The translation was done through Yandex Translator. https://translate.yandex.ru]
---
+++ run_git.cmd
@@ -0,0 +1,222 @@
+@echo off
+rem setlocal
+setlocal enableextensions
+setlocal enabledelayedexpansion
+if errorlevel 1 (
+     echo unable to enable extensions
+     goto failure
+ )
+
+rem if "%~1" == "" goto help
+rem if "%~2" == "" goto help
+if "%~1" == "help" (
+	:help
+	echo ...
+	echo Please use %~nx0 ^<url^> ^<nametosave^> ^<namelogfile^>
+	echo ...
+	exit 0
+	goto end
+)
+
+set file_log=%~dp0%~n0.log
+call :LOG_DT "..."
+call :LOG_DT "Logging to file - %file_log%"
+call :LOG_DT "... START %0"
+
+if "%~1" == "init" (
+ if not "%~2" == "" (
+ call :FINDGIT
+ "%GITEXE%" --version
+ call :FINDGITHUBCLI
+ "%GHEXE%" --version
+
+ call :GITINIT %~2
+ ) else (
+ call :LOG_DT "ERROR ..."	
+ echo Please run '%~nx0 init ^<gitname^>'
+ goto :end
+ )
+)
+
+call :CHECKGIT
+
+call :FINDGIT
+"%GITEXE%" --version
+call :FINDGITHUBCLI
+"%GHEXE%" --version
+
+set ROOTDIR=%CD%
+call :LOG_DT "ROOTDIR = '%ROOTDIR%' ..."
+call :CHANGEDIR %ROOTDIR%
+
+if "%~1" == "remote" (
+call :GITREMOTE
+) else (
+call :GITAUTOCOMMIT
+)
+
+
+:END
+:FAILURE
+call :LOG_DT "ERRORLEVEL %ERRORLEVEL%"
+call :LOG_DT "END %0 ..."
+exit 0
+goto :eof
+
+rem ---------------------------------------------------------------------------------------
+
+:CHECKGIT
+call :LOG_DT "[32mCHECK: GIT repositories ..."
+if exist ".git" (
+call :LOG_DT "GIT EXIST - OK ..."
+"%GITEXE%" status
+call :LOG_DT "ERRORLEVEL %ERRORLEVEL%"
+) else (
+call :LOGERROR "ERROR: no GIT repositories"
+echo [34mPlease run '%~nx0 init ^<gitname^>'[0m
+goto :FAILURE
+)
+goto :eof
+
+rem ==========
+:FINDGIT
+call :LOG_DT "FIND GIT ..."
+FOR %%i IN ("git.exe") DO SET GITEXE=%%~$PATH:i
+IF EXIST "%GITEXE%" @(
+	call :LOG_DT "GIT.EXE = '%GITEXE%'"
+) else (
+call :LOGERROR "Git.exe no found"
+goto :FAILURE
+)
+goto :eof
+
+rem ==========
+:FINDGITHUBCLI
+call :LOG_DT "FIND GITHUBCLI ..."
+for %%i in ("gh.exe", "C:\Program Files (x86)\GitHub CLI\gh.exe", "C:\Program Files\GitHub CLI\gh.exe") do (
+rem echo File '%%~i'
+if exist "%%~i" (
+ set GHEXE=%%~i
+ call :LOG_DT "GH.EXE = '!GHEXE!'"
+ goto :eof
+ rem  ) else (
+ rem  call :LOG_DT "%%i no found"
+ )
+)
+call :LOGERROR "GitHubCli no found"
+goto :FAILURE
+goto :eof
+
+rem ==========
+:GITINIT
+call :LOG_DT "CREATE ..."
+call :CREATEDIR "mywingit%~1"
+call :CHANGEDIR "mywingit%~1"
+call :LOG_DT "COPY FILES ..."
+for %%I in ( "LICENSE.md", "README.md", ".gitignore ", "run_git.cmd" ) do (
+	echo File %%~I
+	if not exist "%%~I" (
+		copy "..\%%~I" "%%~I"
+		)
+	)
+
+call :LOG_DT "INIT GIT ..."
+"%GITEXE%" init
+"%GITEXE%" add .
+"%GITEXE%" commit -m "first commit"
+"%GITEXE%" branch -M master
+"%GITEXE%" remote add origin https://github.com/VArtamonov/mywingit%~1.git
+
+rem "%GHEXE%" auth login --web
+rem "%GHEXE%" auth status
+
+call :LOG_DT "ERRORLEVEL %ERRORLEVEL%"
+"%GHEXE%" repo create --confirm --public --description "My Repo 'mywingit%~1'"
+
+goto :eof
+
+rem ==========
+:GITAUTOCOMMIT
+call :LOG_DT "ADD ALL FILES ..."
+"%GITEXE%" add .
+
+call :LOG_DT "ADD AUTO COMMIT ..."
+call :GET_DT
+call :LOG_DT "Create timestamp %dt%"
+"%GITEXE%" commit -a -m "Auto commit '%dt%'"
+
+call :LOG_DT "GIT STATUS ..."
+"%GITEXE%" status
+goto :eof
+
+rem ==========
+:GITREMOTE
+call :LOG_DT "GIT PUSH REMOTE ..."
+"%GITEXE%" push -u origin master
+goto :eof
+
+rem ==========
+:CHANGEDIR
+rem call :LOG_DT "CHDIR '%~1' ..."
+cd %~1
+call :LOG_DT "CHDIR '%CD%' ..."
+goto :eof
+
+rem ==========
+:CREATEDIR
+call :LOG_DT "DIR '%~1' ..."
+if not exist %~1 (
+call :LOG_DT "CREATE dir '%~1' ..."
+mkdir %~1
+)
+goto :eof
+
+
+rem ==========
+rem %1
+rem %2
+:CREATELINKDIR
+if exist "%~2" (
+	if not exist  "%~1" (
+		echo CREATE %~1
+		mklink /d "%~1" "%~2"
+	) else echo "%~1" - EXIST
+) else echo "%~2" - NO EXIST
+goto :eof
+
+rem ==========
+rem %1 "%~1"
+rem call :LOG Logtxt
+:LOG
+echo %~1
+echo %~1 >> %file_log%
+goto :eof
+
+:LOGERROR
+call :LOG_DT [31m%1
+goto :eof
+
+rem ==========
+:LOG_DT
+rem echo [37m
+set t1=%TIME:~0,1%
+if "%t1%" == " " (
+ set dt=%DATE:~6,4%.%DATE:~3,2%.%DATE:~0,2% 0%TIME:~1,1%:%TIME:~3,2%:%TIME:~6,2%
+) else (
+ set dt=%DATE:~6,4%.%DATE:~3,2%.%DATE:~0,2% %TIME:~0,2%:%TIME:~3,2%:%TIME:~6,2%
+)
+echo %dt% %~1[0m
+echo %dt% %~1 >> %file_log%
+goto :eof
+
+rem ==========
+:GET_DT
+set t1=%TIME:~0,1%
+set mdate=%DATE:~6,4%.%DATE:~3,2%.%DATE:~0,2%
+if "%t1%" == " " (
+ set mtime=0%TIME:~1,1%:%TIME:~3,2%:%TIME:~6,2%
+) else (
+ set mtime=%TIME:~0,2%:%TIME:~3,2%:%TIME:~6,2%
+)
+set dt='%mdate% %mtime%'
+goto :eof
