ac56b4d0ee0d2a094c0a3b98bf67ca81f1f021f2
VArtamonov <avv200071@mail.ru>
2024-04-16 12:45:29 +0300

    Auto commit '2024.04.16 12:45:29'

--- run_git.cmd
+++ run_git.cmd
@@ -1,56 +1,42 @@
 @echo off
-title "Работа с репозитариеми Git ..."
 setlocal
 setlocal enableextensions
 setlocal enabledelayedexpansion
 
-if "%1"=="help" (
-
-:help
-	echo ...
-	echo Please use
-	echo %~nx0 ^<command^>
-	echo 	help   - эта помощь
-	echo 	create ^<folder^> 	- создание локального репозитария в папке ^<folder^> ^(в родительском каталоге^),
-	echo 				  копирует все необходимое, после создания все вызовы надо делать из ^<folder^>
-	echo 	createmaster 		- создание главного репозитария на GitHub, для хранения этих утилит
-	echo 	createhub		- создание удаленного репозитария на GitHub, для хранения созданного
-	echo 	remote			- отправляет все изменения в удаленный репозитария на GitHub
-	echo 				- без параметров дедает автокоммит в текущей датой и временим
-	exit 0
-	goto end
-)
-
-
 set file_log=%~dp0%~n0.log
-call :LOG_DT "..."
-call :LOG_DT "Logging to file - %file_log%"
-call :LOG_DT "... START %0"
+set file_name=%~n0
 
+rem echo ┌───────────────┬─────┬───────────────────────────────────────────────────────────────────────
+call :LOGLINE1
+call :LOGSTART "START '%~0'"
+title "%~n0 - Работа с PostgreSQL ..."
+call :LOGINFO "Log file - '%file_log%'"
+call :LOGINFO "Текущей каталог: '%CD%'"
+call :LOGINFO "Случайное десятичное число: %RANDOM%"
+
+call :LOGLINE2
+call :LOGINFO  "TEST INFO  ..."
+call :LOGERROR "TEST ERROR ..."
+
+call :LOGLINE2
 rem root dir
 set ROOTDIR=%CD%
-call :LOG_DT "ROOTDIR = '%ROOTDIR%' ..."
-CALL :CHANGEDIR %ROOTDIR%
+call :LOGINFO "ROOTDIR = '%ROOTDIR%' ..."
+call :CHANGEDIR %ROOTDIR%
 
-rem Read config file
-echo ==========================================================================================
-set MYINI=%~dp0%~n0.ini
-if exist !MYINI! (
-call :LOG_DT "CONFIG = !MYINI! ..."
-FOR /F "eol=; tokens=1,2 delims=: " %%a in (!MYINI!) do (
-rem echo %%a %%b
-if "%%a" == "UserName" (
-set USERNAME=%%b
-call :LOG_DT "USERNAME= '!USERNAME!' ..."
-) else ( 
-call :LOGERROR "Config file !MYINI! bad ..."
-goto :FAILURE
-)
-)
-) else (
-call :LOGERROR "Config file !MYINI! no found ..."
-goto :FAILURE
-)
+call :LOGLINE2
+call :READINIFILE1 !file_name!
+
+call :LOGLINE2
+call :LOGINFO "ПОИСК УТИЛИТ ..."
+
+call :FINDZIP
+
+call :FINDWGET
+rem set filenameout=%~0
+rem call :MAKENEWFILENAME %filenameout%
+rem call :LOGDEBUG "Filename backup = '%filenameout1%'"
+rem call :WGETLOAD "%git_path%" "%filenameout1%"
 
 call :FINDGIT
 rem "%GITEXE%" --version
@@ -58,6 +44,60 @@ rem "%GITEXE%" --version
 call :FINDGITHUBCLI
 rem "%GHEXE%" --version
 
+
+rem set ENVROOTDIR=%CD%
+rem call :CHANGEDIR %ENVROOTDIR%
+rem set PIPEXE=%ENVROOTDIR%\.venv\Scripts\pip.exe
+rem set PYTHONEXE=%ENVROOTDIR%\.venv\Scripts\python.exe
+rem call :LOGINFO "ENVROOTDIR = '%ENVROOTDIR%'"
+rem call :LOGINFO "PIPEXE     = '%PIPEXE%'"
+rem call :LOGINFO "PYTHONEXE  = '%PYTHONEXE%'"
+
+
+call :LOGLINE2
+
+rem echo .
+rem call %ROOTDIR%\.venv\Scripts\activate.bat 
+
+if "%~1" == "" goto :info
+if "%~1" == "info" goto :info
+if "%~1" == "help" goto :help
+if "%~1" == "check" goto :check
+if "%~1" == "create" goto :create
+if "%~1" == "autocommint" goto :autocommint
+
+
+
+if "%~1" == "createmaster" (
+ call :GITCREATE master
+ call :GITREMOTE
+ goto :end
+)
+
+
+if "%~1" == "createhub" (
+ call :GITCREATEHUB
+ goto :end
+)
+
+
+if "%~1" == "remote" (
+ call :GITREMOTE
+ goto :end
+)
+
+
+goto :help
+
+
+:check
+echo .
+call :GITCHECK
+echo .
+goto :end
+
+
+:create
 if "%~1" == "create" (
  if not "%~2" == "" (
 
@@ -66,68 +106,122 @@ if "%~1" == "create" (
  call :GITCREATE %~2
  
  ) else (
- call :LOG_DT "ERROR ..."	
+ call :LOGERROR "ERROR ..."	
  echo Please run '%~nx0 init ^<gitname^>'
  goto :end
  )
 )
-
-rem -----
-if "%~1" == "createmaster" (
-call :GITCREATE master
-call :GITREMOTE
+echo .
 goto :end
-)
-
-call :GITCHECK
-
-if "%~1" == "createhub" (
-call :GITCREATEHUB
-goto :end
-)
-
-if "%~1" == "remote" (
-call :GITREMOTE
-goto :end
-)
 
 
+:autocommint
 call :GITAUTOCOMMIT
+echo .
+goto :end
+
+:info
+call :LOGINFO "INFO:"
+echo .
+"%GITEXE%" --version
+echo .
+"%GHEXE%" --version
+echo .
+goto :end
+
+
+:help
+ rem ==============================================================================================
+
+ rem call :LOGLINE2
+ call :LOGINFO "Использование:"
+ call :LOGINFO "    '%~0' [КОМАНДА] [ПАРАМЕТРЫ] ..."
+ call :LOGINFO " "
+ call :LOGINFO "Общие параметры:"
+ call :LOGINFO "    help                - Показать эту справку и выйти"
+ call :LOGINFO "    create folder       - создание локального репозитария в папке folder (в родительском каталоге"
+ call :LOGINFO "                          копирует все необходимое, после создания все вызовы надо делать из folder"
+ call :LOGINFO "    createmaster        - создание главного репозитария на GitHub, для хранения этих утилит"
+ call :LOGINFO "    createhub           - создание удаленного репозитария на GitHub, для хранения созданного "
+ call :LOGINFO "    remote              - отправляет все изменения в удаленный репозитария на GitHub"
+ call :LOGINFO "    autocommint         - без параметров дедает автокоммит в текущей датой и временим"
+ call :LOGINFO "                        - без параметров выводит информацию"
+ call :LOGINFO " "
+
+ rem call :LOGLINE2
+
+goto :end
 
 :END
-:FAILURE
-call :LOG_DT "ERRORLEVEL %ERRORLEVEL%"
-call :LOG_DT "END %0 ..."
+call :LOGLINE2
+call :LOGINFO "ERRORLEVEL %ERRORLEVEL%"
+call :LOGINFO "END '%~0' ..."
+call :LOGLINE0
 exit 0
 goto :eof
 
-rem ---------------------------------------------------------------------------------------
+:FAILURE
+call :LOGLINE2
+call :LOGERROR "ERRORLEVEL %ERRORLEVEL%"
+call :LOGERROR "END '%~0' ..."
+call :LOGLINE0
+exit 1
+goto :eof
 
+rem ========== Read SET from ini file ==========
+:READINIFILE1
+rem Read config file
+call :LOGINFO "Read config file ..."
+set MYINI=%~1.ini
+if exist !MYINI! (
+ call :LOG_DT "CONFIG = !MYINI! ..."
+ for /F "usebackq eol=; tokens=1,2 delims=, " %%a in (!MYINI!) do (
+  set %%a=%%b
+  if "%%a" == "UserName" (
+   set USERNAME=%%b
+   call :LOG_DT "USERNAME= '!USERNAME!' ..." 
+   ) else (
+   call :LOG_DT "%%a = '%%b' ..."
+   )
+  )
+ ) else (
+  call :LOGERROR "Config file !MYINI! no found ..."
+  goto :FAILURE
+  )
+goto :eof
+
+
+rem ABBALibraryGITStart
+rem ==============================================================================================
+
+rem ==========
 :GITCHECK
 call :LOGINFO "CHECK: GIT repositories ..."
 set CDIR=%CD%..
-call :LOG_DT "CDIR=%CDIR%"
+call :LOG "CDIR=%CDIR%"
 if exist ".git" (
-call :LOG_DT "GIT EXIST - OK ..."
-rem "%GITEXE%" status
-call :LOG_DT "ERRORLEVEL %ERRORLEVEL%"
+ call :LOGINFO "GIT EXIST - OK ..."
+ rem "%GITEXE%" status
+ call :LOG "ERRORLEVEL %ERRORLEVEL%"
+ exit /b 1
 ) else (
-call :LOGERROR "ERROR: no GIT repositories"
-echo [34mPlease run '%~nx0 init ^<gitname^>'[0m
-goto :FAILURE
+ call :LOGERROR "ERROR: no GIT repositories"
+ echo [34mPlease run '%~nx0 init ^<gitname^>'[0m
+ goto :FAILURE
 )
 goto :eof
+
 
 rem ==========
 :FINDGIT
 call :LOGINFO "FIND GIT ..."
 FOR %%i IN ("git.exe") DO SET GITEXE=%%~$PATH:i
 IF EXIST "!GITEXE!" (
-	call :LOG_DT "GIT.EXE = '!GITEXE!'"
+	call :LOG "GIT.EXE = '!GITEXE!'"
 	goto :eof
 ) else (
 call :LOGERROR "Git.exe no found"
-call :LOG_DT "ERRORLEVEL %ERRORLEVEL%"
+call :LOG "ERRORLEVEL %ERRORLEVEL%"
 goto :FAILURE
 )
 goto :eof
@@ -139,17 +233,20 @@ for %%i in ("gh.exe", "C:\Program Files (x86)\GitHub CLI\gh.exe", "C:\Program Fi
 rem echo File '%%~i'
 if exist "%%~i" (
  set GHEXE=%%~i
- call :LOG_DT "GH.EXE = '!GHEXE!'"
+ call :LOG "GH.EXE = '!GHEXE!'"
  goto :eof
  rem  ) else (
  rem  call :LOG_DT "%%i no found"
  )
 )
 call :LOGERROR "GitHubCli no found"
-call :LOG_DT "ERRORLEVEL %ERRORLEVEL%"
+call :LOG "ERRORLEVEL %ERRORLEVEL%"
 goto :FAILURE
 
 goto :eof
+
+
+rem - GIT Command
 
 rem ==========
 :GITCREATE
@@ -230,10 +327,44 @@ call :LOG_DT "ERRORLEVEL %ERRORLEVEL%"
 goto :eof
 
 
+rem ABBALibraryGITEnd
+
+
+rem ABBALibraryFilesStart
+rem ==============================================================================================
+
+rem ==========
+:FINDZIP
+call :FINDFILE "7z.exe" "ZIPEXE" "C:" "C:\Program Files\7-Zip"
+rem "%ZIPEXE%"
+goto :eof
+
+rem ==========
+:FINDFILE
+call :LOGINFO "FIND '%~1' ..."
+echo off
+for /D %%k in (%3 %4 %5 %6 %7 %8 %9) do (
+ rem for '%~1' %%i in ('%~2') do (
+ set z1=%%~k\%~1
+ rem echo !z1!
+ if exist !z1! (
+   set %~2=!z1!
+   call :LOG "FOUND %~1 = '!%~2!'"
+   goto :eof
+ )
+)
+rem ) else (
+  call :LOGERROR "'%~2' no found"
+  call :LOGERROR "ERRORLEVEL %ERRORLEVEL%"
+  goto :FAILURE
+rem )
+echo off
+goto :eof
+
 rem ==========
 :CHANGEDIR
 rem call :LOG_DT "CHDIR '%~1' ..."
-cd %~1
+cd "%~1"
 call :LOG_DT "CHDIR '%CD%' ..."
 goto :eof
 
@@ -242,7 +373,7 @@ rem ==========
 call :LOG_DT "DIR '%~1' ..."
 if not exist %~1 (
 call :LOG_DT "CREATE dir '%~1' ..."
-mkdir %~1
+mkdir "%~1"
 )
 goto :eof
 
@@ -271,37 +402,170 @@ call :LOG_DT "ROOTDIR = '%ROOTDIR%' ..."
 goto :eof
 
 rem ==========
-rem %1 "%~1"
-rem call :LOG Logtxt
+:CHECKDIR
+call :LOG_DT "CHECK DIR '%~1' ..."
+if not exist "%~1" (
+call :LOGERROR "DIR '%~1' - NOT EXIST ..."
+exit /b 0
+) else (
+call :LOGINFO "DIR '%~1' - OK"
+exit /b 1
+)
+goto :eof
+
+rem ABBALibraryFilesEnd
+
+rem ---------------------------------------------------------------------------------------
+rem ABBALibraryCmdWgetStart
+
+rem ==========
+rem find wget
+:FINDWGET
+call :LOGINFO "FIND WGET ..."
+rem call :LOG "Find wget.exe"
+for %%i in ("%CD%\wget.exe","C:\Windows\wget.exe","C:\Program Files\GnuWin32\bin\wget.exe","C:\Program Files (x86)\GnuWin32\bin\wget.exe","C:\Windows\system32\wget.exe") do (
+ rem call :LOGINFO "%%~i"
+ if exist "%%~i" (
+  set wgetexe=%%~i
+  call :LOG_DT "WGET.EXE = '!wgetexe!'"
+  goto :eof
+  )
+ )
+  call :LOGERROR "wget.exe no found"
+  call :LOG_DT "ERRORLEVEL %ERRORLEVEL%"
+  goto :FAILURE
+rem )
+goto :eof
+
+rem ==========
+rem %1 URL
+rem %2 FILEOUT
+:WGETLOAD
+echo off
+call :LOGDEBUG "WGET LOAD '%~1' to '%~2' ..."
+
+if "%~1"=="" (
+ call :LOGERROR "WGETLOAD 1"
+ goto :eof
+)
+
+if "%~2"=="" (
+ call :LOGERROR "WGETLOAD 2"
+ goto :eof
+)
+
+ rem %wgetexe% --verbose --show-progress "https://downloads.rclone.org/version.txt" --append-output="%file_log%" --output-document="%rclonecurrent_version%"
+ "%wgetexe%" --verbose --show-progress --hsts-file=wget.hsts.txt --save-cookies=wget.cookies.txt --load-cookies=wget.cookies.txt --keep-session-cookies "%~1" --append-output="%file_log%" --output-document="%~2"
+
+
+echo off
+goto :eof
+
+:MAKENEWFILENAME
+rem ==========
+rem %1 FILENAME
+call :GET_DT1
+rem echo mdate='!mdate!'
+rem echo mtime='!mtime!'
+set filenameout1=%~n1_!mdate!!mtime!%~x1
+goto :eof
+
+rem ABBALibraryCmdWgetEnd
+
+
+
+rem ABBALibraryCmdLogStart
+rem ==============================================================================================
+
+:LOGLINE0
+echo └───────────────────┴─────┴─────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────
+goto :eof
+
+:LOGLINE1
+echo ┌───────────────────┬─────┬─────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────
+goto :eof
+
+:LOGLINE2
+echo ├───────────────────┼─────┼─────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────
+goto :eof
+
+rem ==========
 :LOG
-echo %~1
-echo %~1 >> %file_log%
+call :LOGSTR "INFO " "%~1"
+call :LOGSCR  "│%dt%│%tlogstr1%│%tlogstr2%" "[37m"
+call :LOGFILE "%dt% %tlogstr1% %tlogstr2%"
 goto :eof
 
 rem ==========
 :LOGINFO
-call :LOG_DT "[32m%~1"
+call :LOGSTR "INFO " "%~1"
+call :LOGSCR  "│%dt%│%tlogstr1%│%tlogstr2%" "[32m"
+call :LOGFILE "%dt% %tlogstr1% %tlogstr2%"
+goto :eof
+
+rem ==========
+:LOGDEBUG
+call :LOGSTR "INFO " "%~1"
+call :LOGSCR  "│%dt%│%tlogstr1%│%tlogstr2%" "[33m"
+call :LOGFILE "%dt% %tlogstr1% %tlogstr2%"
 goto :eof
 
 rem ==========
 :LOGERROR
-call :LOG_DT "[31m%~1"
+call :LOGSTR "ERROR" "%~1"
+call :LOGSCR  "│%dt%│%tlogstr1%│%tlogstr2%" "[31m"
+call :LOGFILE "%dt% %tlogstr1% %tlogstr2%"
+
 goto :eof
 
-rem ==========
-:LOG_DT
-rem echo [37m
-set t1=%TIME:~0,1%
-if "%t1%" == " " (
- set dt=%DATE:~6,4%.%DATE:~3,2%.%DATE:~0,2% 0%TIME:~1,1%:%TIME:~3,2%:%TIME:~6,2%
-) else (
- set dt=%DATE:~6,4%.%DATE:~3,2%.%DATE:~0,2% %TIME:~0,2%:%TIME:~3,2%:%TIME:~6,2%
+rem %1 "%~1"
+rem call :LOG Logtxt
+:LOG
+echo ... %1[0m
+echo ... %1 >> "%file_log%"
+goto :eof
+
+rem %1 "%~1"
+rem call :LOG Logtxt
+:LOGSCR
+if "%~2"=="" (
+echo %~1[0m
+) else if not "%~2"=="" (
+echo %~2%~1[0m
 )
-echo %dt% %~1[0m
-echo %dt% %~1 >> %file_log%
 goto :eof
 
-rem ==========
+rem %1 "%~1"
+rem call :LOG Logtxt
+:LOGFILE
+echo "%~1" >> "%file_log%"
+goto :eof
+
+rem %1 "%~1"
+rem %2 "%~2"
+rem call :LOGSTR INFO LogStr
+:LOGSTR
+call :GET_DT
+set dt=%mdate% %mtime%
+set tlogstr1=%~1
+set tlogstr2=%~2
+rem echo %tlogstr%
+goto :eof
+
+:LOG_DT
+call :LOGSTR  "     " "%~1"
+call :LOGSCR  "│%dt%│%tlogstr1%│%tlogstr2%"
+call :LOGFILE "%dt% %tlogstr1% %tlogstr2%"
+goto :eof
+
+:LOGSTART
+call :LOGSTR  "     " "%~1"
+call :LOGFILE "%dt% --------------------------------------------------------------------------------------------------------------------"
+call :LOGSCR  "│%dt%│%tlogstr1%│%tlogstr2%"
+call :LOGFILE "%dt% %tlogstr1% %tlogstr2%"
+goto :eof
+
+rem get data and time
 :GET_DT
 set t1=%TIME:~0,1%
 set mdate=%DATE:~6,4%.%DATE:~3,2%.%DATE:~0,2%
@@ -310,5 +574,19 @@ if "%t1%" == " " (
 ) else (
  set mtime=%TIME:~0,2%:%TIME:~3,2%:%TIME:~6,2%
 )
-set dt='%mdate% %mtime%'
+rem set dt=%mdate%-%mtime%
 goto :eof
+
+rem get data and time
+:GET_DT1
+set t1=%TIME:~0,1%
+set mdate=%DATE:~6,4%%DATE:~3,2%%DATE:~0,2%
+if "%t1%" == " " (
+ set mtime=0%TIME:~1,1%%TIME:~3,2%%TIME:~6,2%
+) else (
+ set mtime=%TIME:~0,2%%TIME:~3,2%%TIME:~6,2%
+)
+rem set dt=%mdate%-%mtime%
+goto :eof
+
+rem ABBALibraryCmdLogEnd
